c68df57fcb1c612b9a5c5ab3bcce504ff735620a
SoullessSoldier <48070769+SoullessSoldier@users.noreply.github.com>
2021-06-10 13:06:02 +0200

    Update script.js

--- lesson10_adv/js/script.js
+++ lesson10_adv/js/script.js
@@ -6,8 +6,9 @@ const list = document.getElementsByTagName('ul')[0];
 btnAddLiItem.addEventListener('click',()=>{
     let newItem = document.createElement('li');
     let inputValue = inputItem.value;
-    newItem.textContent = inputValue;
-    list.appendChild(newItem);
-    inputItem.value = '';
-
+    if (inputValue.trim()){
+      newItem.textContent = inputValue;
+      list.appendChild(newItem);
+      inputItem.value = '';
+    } else (alert('Введите непустое значение!'))
 })
